7e12f4a6cb554e76863088fec82eeefc8c797837
abinash0013 <74831561+abinash0013@users.noreply.github.com>
2022-07-11 14:03:48 +0530

    Update Banner.js

--- src/pages/Header/Banner/Banner.js
+++ src/pages/Header/Banner/Banner.js
@@ -42,7 +42,7 @@ function Banner() {
                                         type="submit"
                                         className="search-form-btn"
                                     >
-                                        Search
+                                        Searchs
                                     </button>
                                 </form>
                             </div>
